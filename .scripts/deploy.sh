d8f7f9c3b2368f5adeb15c94971da5faffc4a6e9
iamyashsinghh <iamyashsinghh@gmail.com>
2024-04-07 21:24:13 +0530

    ok

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -4,15 +4,19 @@ set -e
 echo "Deployment started..."
 
 git pull
+
 echo "New changes copied to server !"
 
 echo "Installing Dependencies..."
+
 npm install --yes
 
 echo "Creating Production Build..."
+
 npm run build
 
 echo "PM2 Reload"
-pm2 reload 8
+
+pm2 reload 9
 
 echo "Deployment Finished!"

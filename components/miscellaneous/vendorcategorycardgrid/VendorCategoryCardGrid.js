897aa6756727c8c0f54de9486045e3ca6d7fa306
iamyashsinghh <iamyashsinghh@gmail.com>
2024-04-18 17:15:45 +0530

    ok

--- components/miscellaneous/vendorcategorycardgrid/VendorCategoryCardGrid.js
+++ components/miscellaneous/vendorcategorycardgrid/VendorCategoryCardGrid.js
@@ -13,7 +13,7 @@ export default function VendorCategoryCardGrid (){
                 <div className="card-container">
                     <VendorCategoryCard bgColor={"#FAF3F0"} imgUrl={'/vendor-cat/photographer.jpeg'} title={"Photographer"} desc={"Photgrapher & Videographer"} url={"/best-wedding-photographers/delhi/all"}/>
                     <VendorCategoryCard bgColor={"#FAF0D7"} imgUrl={'/vendor-cat/makeup.jpeg'} title={"Makeup Atrist"} desc={"Wedding Makeup Artist"}  url={"/top-makeup-artists/delhi/all"}/>
-                    <VendorCategoryCard bgColor={"#F7FFE5"} imgUrl={'/vendor-cat/mehndi.jpeg'} title={"Mehndi Artist"} desc={"Wedding Mehndi Artist"}  url={"/best-mehndi-artists/delhi/all"}/>
+                    <VendorCategoryCard bgColor={"#F7FFE5"} imgUrl={'/vendor-cat/mehndi-artist.jpeg'} title={"Mehndi Artist"} desc={"Wedding Mehndi Artist"}  url={"/best-mehndi-artists/delhi/all"}/>
                     <VendorCategoryCard bgColor={"#F9F5F6"} imgUrl={'/vendor-cat/wedding-decorator.jpeg'} title={"Decorators"} desc={"Decorators "}  url={"/best-decorators/delhi/all"}/>
                     <VendorCategoryCard bgColor={"#F0EDD4"} imgUrl={'/vendor-cat/wedding-transport.jpeg'} title={"Wedding Transport"} desc={"Transport for Wedding"}  url={"/wedding-transportation-and-vintage-cars/delhi/all"}/>
                     <VendorCategoryCard bgColor={"#E3F4F4"} imgUrl={'/vendor-cat/band-baja.jpeg'} title={"Band-Baja"} desc={"Band-Baja"}  url={"/band-baja-ghodiwala/delhi/all"}/>

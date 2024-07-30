5cfe4c054a20ee13173977960c37716c2a4521d2
Yash Singh <iamyashsinghh@gmail.com>
2024-07-30 14:31:36 +0530

    ok commmit

--- middleware.js
+++ middleware.js
@@ -6,7 +6,8 @@ function middleware(request) {
 
   if (host && host.startsWith('www.')) {
     url.host = host.slice(4); // Remove 'www.' from the host
-    url.protocol = request.nextUrl.protocol; // Preserve the protocol (http or https)
+    url.protocol = 'https'; // Always use HTTPS
+    url.port = ''; // Ensure the port is not included in the redirect URL
     return NextResponse.redirect(url);
   }
 

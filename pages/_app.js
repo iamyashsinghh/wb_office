f1c2024ddf44dacf6a48e87bf666f9476304eb88
Yash Singh <iamyashsinghh@gmail.com>
2024-08-13 14:55:04 +0530

    ok commit

--- pages/_app.js
+++ pages/_app.js
@@ -51,43 +51,6 @@ export default function App({ Component, pageProps }) {
         <meta name="twitter:image" content="https://weddingbanquets.in/twitter-img.png" />
       </Head>
       <Layout>
-        {/* Google Tag Manager (noscript) */}
-        <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-M8J8X27"
-            height="0"
-            width="0"
-            style={{ display: "none", visibility: "hidden" }}
-          ></iframe>
-        </noscript>
-        {/* End Google Tag Manager (noscript) */}
-
-        {/* Google Tag Manager */}
-        <Script
-          id="my-script"
-          strategy="beforeInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src= 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f); })(window,document,'script','dataLayer','GTM-M8J8X27');`
-          }}
-        />
-        {/* End Google Tag Manager */}
-
-        {/* Google tag (gtag.js) */}
-        {/* old g tag  */}
-        <Script
-          id="my-script2"
-          src="https://www.googletagmanager.com/gtag/js?id=AW-11325854315"
-          strategy="afterInteractive"
-        />
-        <Script strategy="afterInteractive" id="my-script3">
-          {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', 'AW-11325854315');
-        `}
-        </Script>
-
         <a
           href="https://api.whatsapp.com/send?phone=918882198989&text=Hi"
           className="whatsapp-button"
@@ -107,3 +70,41 @@ export default function App({ Component, pageProps }) {
     </MyContextProvider>
   );
 }
+
+
+ {/* Google Tag Manager (noscript) */}
+        {/* <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-M8J8X27"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript> */}
+        {/* End Google Tag Manager (noscript) */}
+
+        {/* Google Tag Manager */}
+        {/* <Script
+          id="my-script"
+          strategy="beforeInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src= 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f); })(window,document,'script','dataLayer','GTM-M8J8X27');`
+          }}
+        /> */}
+        {/* End Google Tag Manager */}
+
+        {/* Google tag (gtag.js) */}
+        {/* old g tag  */}
+        {/* <Script
+          id="my-script2"
+          src="https://www.googletagmanager.com/gtag/js?id=AW-11325854315"
+          strategy="afterInteractive"
+        />
+        <Script strategy="afterInteractive" id="my-script3">
+          {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'AW-11325854315');
+        `}
+        </Script> */}

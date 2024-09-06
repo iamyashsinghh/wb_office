168f430b4819e0bb12eb988e46b85496ca61af1b
Yash Singh <iamyashsinghh@gmail.com>
2024-09-06 14:16:11 +0530

    ok commit

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,15 +1,36 @@
-//Redirect to the blog site, The will handle /blog => blog.weddingbanquets.in
+import BlogList from "@/components/blog/bloglist/BlogList";
+import Header from "@/components/layout.js/Header";
+import Head from "next/head";
+import styled from "styled-components";
 
- const App = ()=> null;
+export default function Blog({ data }) {
+  return (
+    <>
+      <Header />
+      <Head>
+        <title>Tips and Ideas for your Wedding | Wedding Banquets</title>
+        <meta name="description" content='Discover expert tips and creative ideas for planning an unforgettable wedding. From venue selection to decor inspiration | Wedding Banquets Blog' />
+        <meta name="keywords" content='wedding planning, venue selection, decor inspiration, wedding tips, wedding ideas, wedding banquet, wedding reception, wedding decorations' />
+      </Head>
+      <Wrapper className="section container">
+        <BlogList data={data} />
+      </Wrapper>
+    </>
+  );
+}
 
-export async function getServerSideProps(context) {
-    return {
-      redirect: {
-        destination: 'https://weddingbanquets.in/blog', // the URL you want to redirect to
-        permanent: true, // true for a 301 redirect, false for 302
-      },
-    }
-  }
-  
+export async function getServerSideProps() {
+  const url = `${process.env.NEXT_PUBLIC_SERVER_DOMAIN}/api/blog_list`;
+  let newLists = await fetch(url);
+  let data = await newLists.json();
+  data = data.data;
+  // console.log(data.data)
 
-  export default App;
+  return {
+    props: {
+      data,
+    },
+  };
+}
+
+const Wrapper = styled.section``;
